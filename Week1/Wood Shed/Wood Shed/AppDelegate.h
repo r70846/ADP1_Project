c261e346b528928d1ea519f50980a31742f0163d
Russ Gaspard <russg@fullsail.edu>
2014-09-11 09:36:54 -0400

    added first files

---
+++ Week1/Wood Shed/Wood Shed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wood Shed
+//
+//  Created by Russell Gaspard on 9/11/14.
+//  Copyright (c) 2014 Russell Gaspard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
